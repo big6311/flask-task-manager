d27bb8a1e3631881e71766d7b1fdf12e0202c1d7
Bryan <bryan@example.com>
2025-12-07 03:58:32 -0500

    Fix Flask app entrypoint for Render

--- task_api.py
+++ task_api.py
@@ -114,12 +114,3 @@ def home():
 if __name__ == "__main__":
     load_tasks()
 
-    # When running in the cloud, the platform will set PORT.
-    # Locally it'll default to 5000.
-    port = int(os.environ.get("PORT", 5000))
-
-    app.run(
-        host="0.0.0.0",  # listen on all network interfaces (needed in cloud)
-        port=port,
-        debug=False      # debug=False in production
-    )
